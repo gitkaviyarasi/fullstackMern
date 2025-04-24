8c9b7359d5d3069614c5350aea983a305b20294e
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2025-04-23 21:55:57 -0400

    added login and register page

---
+++ BackendDev/Server/README.md
@@ -0,0 +1,10 @@
+## Back end Development
+ To implement a basic user authentication using Node js and Express that allows users to login with their username and passoword. They can also register with the name and password. We will use MongoDB as the database to store user information. The application will have the following features:
+1. User Registration: Users can register with their name and password.
+2. User Login: Users can log in with their username and password.
+3. User Authentication: The application will authenticate users using JWT (JSON Web Token).
+## Usage
+1. Run npm install to install all the dependencies.
+2. Run npm start to start the server. For development, run npm run dev which tracks the changes in the code and restarts the server automatically.
+3. Used Insomina for testing the routes.
+4.  
---
+++ fullStackMern/client/src/pages/createAccount.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+// import Userdata from "../interfaces/CreateAccount";
+
+import { useNavigate } from "react-router-dom";
+import { createUser } from "../api/createuser";
+import Auth from "../utils/auth";
+
+
+
+const CreateAccount = () => {
+  const [newUser, setnewUser] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newUser) {
+      const data = await createUser(newUser);
+      console.log(data);
+      Auth.login(data.token);
+      navigate("/searchBooks");
+    }
+  };
+
+  const handleTextChange = (e ) => {
+    const { name, value } = e.target;
+    setnewUser((prev) => (prev ? { ...prev, [name]: value } : undefined));
+  };
+
+  return (
+    <div className="create-account">
+      {/* <h2>Create Account</h2> */}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={newUser?.username || ""}
+            onChange={handleTextChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={newUser?.password || ""}
+            onChange={handleTextChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={newUser?.email || ""}
+            onChange={handleTextChange}
+            required
+          />
+        </div>
+        <button type="submit" className="createacc">
+          Create Account
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateAccount;
